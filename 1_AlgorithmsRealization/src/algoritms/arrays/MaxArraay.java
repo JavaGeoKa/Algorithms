ba19b9fa5c99b9e27577cea52ed7ac4d27bda811
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-09-08 16:09:08 +0300

    1

---
+++ 1_AlgorithmsRealization/src/algoritms/arrays/MaxArraay.java
@@ -0,0 +1,20 @@
+package algoritms.arrays;
+
+
+	public class MaxArraay {
+	    public static void main(String[] args) {
+	        int [] mas = new int[12];
+	        int maxNumber = -16;
+	        int maxIndex = 0;
+	 
+	        for (int i = 0; i < mas.length; i++) {
+	            mas[i] = (int)(Math.random()*31)-15;
+	            if(maxNumber <= mas[i]){
+	                maxNumber = mas[i];
+	                maxIndex = i;
+	            }
+	        }
+	        
+	        System.out.println("Индекс максимального элемента: " + maxIndex);
+	    }
+	}
