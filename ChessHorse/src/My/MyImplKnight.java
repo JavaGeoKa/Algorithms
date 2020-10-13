02feaf4e86bcbe669d9b1d1c580e844468a89cb1
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-10-13 19:48:11 +0300

    control quantity

--- ChessHorse/src/My/MyImplKnight.java
+++ ChessHorse/src/My/MyImplKnight.java
@@ -1,91 +1,79 @@
-package Genuis;
+package My;
 
-/**
- *
- * @author peter
- */
-public class Var1 {
+import java.util.Scanner;
+
+public class MyImplKnight {
+	
+	static int numberOfAttempts = 3;
     
     // Параметры Шахматной доски и варианты разрешенных ходов из текущей позиции коня
-    final int deskSize = 8;
+    static final int deskSize = 8;
     boolean deskCell[][] = new boolean[deskSize][deskSize];
-    String deskCellColName[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-    int deskRow[] = new int[deskSize*deskSize];
-    int deskCol[] = new int[deskSize*deskSize];
-    int deskMov1[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+    static int deskRow[] = new int[deskSize*deskSize];      //репозиторий
+    static int deskCol[] = new int[deskSize*deskSize];
+    int deskMov1[] = {-2, -1, 1, 2, 2, 1, -1, -2};			//ходы 
     int deskMov2[] = {1, 2, 2, 1, -1, -2, -2, -1};
-    
-    
+    static int deskRowHelper[] = new int[deskSize*deskSize];      //репозиторий
+    static int deskColHelper[] = new int[deskSize*deskSize];
 
     // Начальная позиция коня и общее количество ходов..
     int i, j;
     int moveNum = 0;
-    int c = 0;
     
-    //
-    Var1() {
-        
-    }
+    static int input = 0;
+    
+    //то
+    MyImplKnight() {}
     
     //
     // Hamilton's Cycle Algorithm..
     //
     void theHamiltonCycle() {
+    	
         int a, b, e;
         /* Пометить клетку как посещенную и запомнить координаты клетки */
-        deskCell[i][j] = true;
+        deskCell[i][j] = true;     
         deskRow[moveNum] = i;
         deskCol[moveNum] = j;
         moveNum++;
-        c++;
 
-        for (a = 0; a < deskMov1.length; a++) {
+        
+        /* Проверить 8 (Буква Г) возможных перемещений коня */
+        for (a = 0; a < deskSize; a++) {                      //проходим по 
             /* Если все ходы сделаны, печатаем их */
             if (moveNum >= deskSize*deskSize) {
-                printDesk();
-//                System.exit(0);
+                
+            	
+            	printDesk();
+            	
+            	System.out.println(input);
+            	
+            	if (--input == 0) {
+                	System.exit(0);
+            	}
+            	continue;
             }
 
+            
             /* Определяем колонку и строку для следующего хода */
             b = i + deskMov1[a];
             e = j + deskMov2[a];
 
             /* Проверяем, что после выполенения хода конь остается на шахматной доске */
-            if (b < 0 || b > deskSize - 1 || e < 0 || e > deskSize - 1) continue;
-            
-//            System.out.println("Avialable move - " + deskCellColName[e] + Integer.toString(b + 1));
-            
+            if (b < 0 || b > deskSize - 1 || e < 0 || e > deskSize - 1) continue; 
+
             /* Проверяем, были ли мы уже в этой клетке */
-            if (deskCell[b][e]) continue;
+            if (deskCell[b][e]) continue; 
             
+            //ToDo
+//            System.out.println("Avialable Move " + a + "   point " + b + e);
             i = b; j = e;
-              
+            //-------------------------
+            
+            
+            
             theHamiltonCycle();
-        }
-        
-        ////============================
-        /* Проверить 8 (Буква Г) возможных перемещений коня */
-//        for (a = 0; a < deskMov1.length; a++) {
-//            /* Если все ходы сделаны, печатаем их */
-//            if (moveNum >= deskSize*deskSize) {
-//                printDesk();
-//                System.exit(0);
-//            }
-//
-//            /* Определяем колонку и строку для следующего хода */
-//            b = i + deskMov1[a];
-//            e = j + deskMov2[a];
-//
-//            /* Проверяем, что после выполенения хода конь остается на шахматной доске */
-//            if (b < 0 || b > deskSize - 1 || e < 0 || e > deskSize - 1) continue; 
-//
-//            /* Проверяем, были ли мы уже в этой клетке */
-//            if (deskCell[b][e]) continue; 
-//              
-//            i = b; j = e;
-//              
-//            theHamiltonCycle();
-//        } 
+        } 
 
         /* Уменьшить счетчик ходов и попробовать сделать следующий ход */
         moveNum--;
@@ -96,27 +84,53 @@ public class Var1 {
         i = deskRow[moveNum];
         j = deskCol[moveNum];
         moveNum++;
+        
+      
+      
+    
     }
+    
+    
+    
+    
+    
+    
+    
+    
     
     void printDesk( ) {
         int a;
         
-        System.out.println(c);
                 
         for (a = 0; a < deskSize*deskSize; a++) {
             System.out.printf("%d%d ",deskCol[a], deskRow[a]);
         }
        
-       
-        
-        System.out.println();
+    
     }
+    
+    
+    
+    
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Var1().theHamiltonCycle();
+    	
+//    	Scanner sc = new Scanner(System.in);
+//        //Вводим предложение в консоль
+//        System.out.println("input quantity resultat: ");
+//        input = Integer.parseInt(sc.nextLine());
+      
+    	MyImplKnight genuis = new MyImplKnight();
+    
+    		input = 5;
+    			
+    		genuis.theHamiltonCycle();
+    		
+		
+       
     }
     
 }
